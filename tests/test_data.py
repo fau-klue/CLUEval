1013690fc2922486eb7dda5fba40cac6970bad60
Bao Minh Doan Dang <minh.ddb@gmail.com>
2025-09-03 13:14:35 +0200

    revised test

--- tests/test_data.py
+++ tests/test_data.py
@@ -18,14 +18,14 @@ def test_converter(p1):
     assert df3.shape[0] != 0
 
     # test required columns
-    df1_required_columns = ['start', 'end', 'layer1', 'domain', 'doc_id', 'text', 'id']
-    assert df1.columns.tolist() == df1_required_columns
+    df1_required_columns = ['start', 'end', 'text', 'layer1', 'doc_id', 'domain', 'id']
+    assert sorted(df1.columns.tolist()) == sorted(df1_required_columns)
 
-    df2_required_columns = ['start', 'end', 'layer2', 'domain', 'doc_id', 'text', 'id']
-    assert df2.columns.tolist() == df2_required_columns
+    df2_required_columns = ['start', 'end', 'text', 'layer2', 'doc_id', 'domain', 'id']
+    assert sorted(df2.columns.tolist()) == sorted(df2_required_columns)
 
-    df3_required_columns = ['start', 'end', 'layer3', 'domain', 'doc_id', 'text', 'id']
-    assert df3.columns.tolist() == df3_required_columns
+    df3_required_columns = ['start', 'end', 'text', 'layer3', 'doc_id', 'domain', 'id']
+    assert sorted(df3.columns.tolist()) == sorted(df3_required_columns)
 
     # test id prefix
     assert df1["id"][0].startswith("reference")
