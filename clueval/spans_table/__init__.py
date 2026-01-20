e52c363aaef521ddb1b7375130f795dea97fdb77
Bao Minh Doan Dang <minh.ddb@gmail.com>
2026-01-20 09:53:04 +0100

    small revisions

--- clueval/spans_table/__init__.py
+++ clueval/spans_table/__init__.py
@@ -1,4 +1,6 @@
 from .join import Join
+from .convert import Convert
+from .match import Match
 from .parser import BioToSpanParser
 from .data import ParsedSpan, SpanComponent, UnifiedSpan, Token
 from.unify import OverlapComponentUnifier, MultiHeadSpanTokenUnifier
