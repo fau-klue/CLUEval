b770140c642e0f399ec4f7df367892af6793e578
Bao Minh Doan Dang <minh.ddb@gmail.com>
2026-01-20 09:51:58 +0100

    For now: extract doc_id from doc_token_id_column

--- clueval/spans_table/parser.py
+++ clueval/spans_table/parser.py
@@ -20,26 +20,24 @@ class BioToSpanParser:
         tokens = []
         # Extract spans from BIO
         for span in self.extract_spans_from_iob(tag_column=tag_column,
-                                                    doc_id_column=doc_id_column
-                                                    ):
+                                                doc_token_id_column=doc_token_id_column
+                                                ):
             spans.append(span)
 
         # Extract tokens from BIO
         if extract_tokens:
             for token in self.extract_tokens_from_iob(n_tag_columns=n_tag_columns,
                                                       doc_token_id_column=doc_token_id_column,
-                                                      domain_column=domain_column,
-                                                      doc_id_column=doc_id_column):
+                                                      domain_column=domain_column):
                 tokens.append(token)
 
         return spans, tokens
 
-    def extract_tokens_from_iob(self, n_tag_columns=1, doc_token_id_column: int=None, domain_column: int=None, doc_id_column: int=None):
+    def extract_tokens_from_iob(self, n_tag_columns=1, doc_token_id_column: int=None, domain_column: int=None):
         """
         :param n_tag_columns:
         :param doc_token_id_column:
         :param domain_column:
-        :param doc_id_column:
 
         """
 
@@ -51,15 +49,12 @@ class BioToSpanParser:
             lines = in_f.readlines()
             for i, line in enumerate(lines):
                 # Extract document id if available
-                current_line = line.strip()
-                if "newdoc id" in current_line:
-                    doc_id = current_line.split("=")[1].strip()
-                else:
-                    if doc_id_column and current_line.strip() != "":
-                        doc_id = current_line.strip().split("\t")[doc_id_column].strip()
-
-                current_line = current_line.split("\t")
+                current_line = line.strip().split("\t")
                 if len(current_line) > 1:
+                    # Extract document id if exists
+                    if doc_token_id_column:
+                        token_id = current_line[doc_token_id_column]
+                        doc_id = "_".join(token_id.strip().split("_")[:2])
                     position += 1
                     # We assume that the tag column is directly adjacent to the token column
                     token = current_line[0]
@@ -68,8 +63,6 @@ class BioToSpanParser:
                     else:
                         label = [re.sub(r"[BI]-", "", tag) for tag in current_line[1:n_tag_columns+1]]
                     # Extract token_id and domain from BIO file if available
-                    if doc_token_id_column is not None:
-                        token_id = current_line[doc_token_id_column]
                     if domain_column is not None:
                         domain = current_line[domain_column].lower()
                     yield Token(position=position,
@@ -79,7 +72,7 @@ class BioToSpanParser:
                                 doc_id=doc_id,
                                 domain=domain)
 
-    def extract_spans_from_iob(self, tag_column=1, doc_id_column: int = None):
+    def extract_spans_from_iob(self, tag_column=1, doc_token_id_column: int = None):
         """
         Extract predicted spans from BIO file.
         Iterate over each line and check whether predicted tag for current lines header is 'O'. If not do:
@@ -90,7 +83,7 @@ class BioToSpanParser:
                 - extract token position as end id
                 - return spans information
         :param tag_column:
-        :param doc_id_column:
+        :param doc_token_id_column:
         """
         with open(self.path_to_file, "r", encoding="utf-8") as in_f:
             # doc_token_id is the predefined token_id in each document while token_id is the token position in the whole dataset
@@ -101,17 +94,8 @@ class BioToSpanParser:
 
             lines = in_f.readlines()
             for i, line in enumerate(lines):
-                # Extract document id if available
-                current_line = line.strip()
-                if "newdoc id" in current_line:
-                    current_doc_id = current_line.split("=")[1].strip()
-                else:
-                    if doc_id_column and current_line.strip() != "":
-                        doc_id = current_line.strip().split("\t")[doc_id_column].strip()
-                        # Check if _doc_id != doc_id
-                        if doc_id != "" and doc_id != current_doc_id:
-                            current_doc_id = doc_id
-                current_line = current_line.split("\t")
+                current_line = line.strip().split("\t")
+
 
                 # Extract next line if possible
                 try:
@@ -121,6 +105,12 @@ class BioToSpanParser:
 
                 # Extract spans based on predicted tags
                 if len(current_line) > 1:
+                    # Extract document id if exists
+                    if doc_token_id_column:
+                        doc_id = "_".join(current_line[doc_token_id_column].strip().split("_")[:2])
+                        # Check if doc_id != current_doc_id
+                        if doc_id != current_doc_id:
+                            current_doc_id = doc_id
                     token_id += 1
                     current_tag = current_line[tag_column]
                     # Start processing line if current tag is not "O"
@@ -142,13 +132,3 @@ class BioToSpanParser:
                                              doc_id=current_doc_id,
                                              head=tag_column)
                             label = None
-
-    @staticmethod
-    def _assign_span_ids(inp_data: pd.DataFrame, prefix: str = "id"):
-        """
-        Assign IDs to annotated spans using the given prefix.
-        :param inp_data: Pandas dataframe with extracted spans
-        :param prefix: IDs prefix
-        """
-        inp_data["id"] = [f"{prefix}{i + 1:06d}" for i in range(inp_data.shape[0])]
-        return inp_data
