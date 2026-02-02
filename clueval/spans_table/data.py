57498a27d7b240f300c779ad99f600338182b083
Bao Minh Doan Dang <minh.ddb@gmail.com>
2026-02-02 15:34:22 +0100

    modified type

--- clueval/spans_table/data.py
+++ clueval/spans_table/data.py
@@ -1,22 +1,25 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class UnifiedSpan:
     start_id: int
     end_id: int
     doc_token_id_start: int
     doc_token_id_end: int
-    text:str
-    label: str|list[str]
+    text: str
+    label: str | list[str]
     doc_id: str
     domain: str
+
 
 @dataclass
 class ParsedSpan:
     start_id: int
     end_id: int
-    doc_id: str
-    head: int|str
+    doc_id: str | None
+    head: int | str
+
 
 @dataclass
 class SpanComponent:
@@ -24,13 +27,12 @@ class SpanComponent:
     end_id: int
     doc_id: str | int
 
+
 @dataclass
 class Token:
     position: int
-    token_id: int|str
+    token_id: int | str | None
     token: str
-    label: str|list[str]
-    doc_id: str
-    domain: str
-
-
+    label: str | list[str]
+    doc_id: str | None
+    domain: str | None
