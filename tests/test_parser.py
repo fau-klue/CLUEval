86ceeec587118abf81edb4352508ee51e474974b
Bao Minh Doan Dang <minh.ddb@gmail.com>
2026-02-24 17:14:13 +0100

    adjusted test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,8 +7,8 @@ def test_bio_to_sentence(p1):
     assert len(pos_to_sent_mapping["token_ids"]) == 8
     assert len(pos_to_sent_mapping["sents"]) == 8
 
-    assert pos_to_sent_mapping["token_ids"][0][0] == 1
-    assert pos_to_sent_mapping["token_ids"][0][-1] == 4
+    assert pos_to_sent_mapping["token_ids"][0][0] == 0
+    assert pos_to_sent_mapping["token_ids"][0][-1] == 3
 
     assert pos_to_sent_mapping["sents"][0][0] == pos_to_sent_mapping["sents"][0][-1]
 
